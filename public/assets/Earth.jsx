456a7862c0eed8030c617a7899d96d65f60f170b
Louistam888 <louistam888@gmail.com>
2023-08-10 22:14:04 -0400

    finished earth

--- public/assets/Earth.jsx
+++ public/assets/Earth.jsx
@@ -1,12 +1,31 @@
 import { useTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
-const Earth = () => {
-  const [earthTexture] = useTexture(["./public/assets/earthDay.jpg"]);
+const Earth = ({ displacementScale }) => {
+  const earthRef = useRef();
+  const [earthTexture, earthNormalMap, earthSpecularMap, earthDisplacementMap] =
+    useTexture([
+      "./public/assets/earthDay.jpg",
+      "./public/assets/earthNormal.jpg",
+      "./public/assets/earthSpecular.jpg",
+      "./public/assets/earthDisplacement.jpg",
+    ]);
+
+  useFrame(() => {
+    earthRef.current.rotation.y += 0.002;
+  });
   return (
-    <mesh>
+    <mesh ref={earthRef}>
       {/* args are radius, x-axis and y axis */}
       <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial map={earthTexture} />
+      <meshPhongMaterial
+        map={earthTexture}
+        normalMap={earthNormalMap}
+        specularMap={earthSpecularMap}
+        displacementMap={earthDisplacementMap}
+        displacementScale={displacementScale}
+      />
     </mesh>
   );
 };
--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useHelper } from "@react-three/drei";
+import { useHelper } from "@react-three/drei";
 import AnimatedStars from "./AnimatedStars";
 import Earth from "../public/assets/Earth";
 import { useRef } from "react";
@@ -8,13 +8,13 @@ const MainContainer = () => {
   const directionalLightRef = useRef();
   const directionalLightRefTwo = useRef();
   //   number is size of helper
-  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, "hotpink");
-  useHelper(directionalLightRefTwo, THREE.DirectionalLightHelper, 1, "hotpink");
+//   useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, "hotpink");
+//   useHelper(directionalLightRefTwo, THREE.DirectionalLightHelper, 1, "hotpink");
 
   return (
     <>
       <color attach="background" args={["black"]} />
-      <OrbitControls />
+    
       <AnimatedStars />
       <directionalLight
         ref={directionalLightRef}
@@ -23,7 +23,7 @@ const MainContainer = () => {
         // color={0xff0000}
       />
       <directionalLight ref={directionalLightRefTwo} position={[0, 0, -10]} />
-      <Earth />
+      <Earth displacementScale={0.15}/>
     </>
   );
 };
