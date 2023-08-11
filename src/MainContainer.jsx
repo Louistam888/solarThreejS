cface50c1d1864b9838a1ac864d2a236f7a82801
Louistam888 <louistam888@gmail.com>
2023-08-10 23:10:17 -0400

    added moon

--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -8,22 +8,27 @@ const MainContainer = () => {
   const directionalLightRef = useRef();
   const directionalLightRefTwo = useRef();
   //   number is size of helper
-//   useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, "hotpink");
-//   useHelper(directionalLightRefTwo, THREE.DirectionalLightHelper, 1, "hotpink");
+  //   useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, "hotpink");
+  //   useHelper(directionalLightRefTwo, THREE.DirectionalLightHelper, 1, "hotpink");
 
   return (
     <>
       <color attach="background" args={["black"]} />
-    
+
       <AnimatedStars />
       <directionalLight
+        castShadow
         ref={directionalLightRef}
         position={[0, 0, 10]}
         intensity={3}
         // color={0xff0000}
       />
-      <directionalLight ref={directionalLightRefTwo} position={[0, 0, -10]} />
-      <Earth displacementScale={0.15}/>
+      <directionalLight
+        castShadow
+        ref={directionalLightRefTwo}
+        position={[0, 0, -10]}
+      />
+      <Earth displacementScale={0.15} />
     </>
   );
 };
---
+++ src/Moon.jsx
@@ -0,0 +1,25 @@
+import { useTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const Moon = () => {
+  const moonRef = useRef();
+  const [moonTexture] = useTexture(["./public/assets/moonTexture.jpg"]);
+
+  useFrame(({clock}) => {
+    //orbit rotation
+    moonRef.current.position.x = Math.sin(clock.getElapsedTime() * 0.8) * 4;
+    moonRef.current.position.z = Math.cos(clock.getElapsedTime() * 0.8) * 4;
+    //axis rotation
+    moonRef.current.rotation.y += 0.002;
+  });
+  return (
+    <mesh castShadow ref={moonRef} position={[4, 0, 0]}>
+      {/* args are radius, x-axis and y axis */}
+      <sphereGeometry args={[0.2, 32, 32]} />
+      <meshPhongMaterial map={moonTexture} />
+    </mesh>
+  );
+};
+
+export default Moon;
